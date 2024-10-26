84cb21fea8e45e11aa26542381ac6bfd8f04847d
Trockii <trocki0555@gmail.com>
2024-10-26 16:27:09 +0200

    komentarze

--- slowa.py
+++ slowa.py
@@ -1,14 +1,16 @@
+#import polskich znaków w konsoli
 import sys
 import codecs
 import re
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
 
-tekst = open("tekst.txt", "r", encoding="utf-8")
-zawartosc = tekst.read().lower()
-zawartosc = re.sub(r'[^\w\s]', '', zawartosc)
-slowa = zawartosc.split()
+#tworzenie zmiennych
+tekst = open("tekst.txt", "r", encoding="utf-8") #połączenie tekstu zewnętrznego
+zawartosc = tekst.read().lower() #połączenie tekstu zewnętrznego
+zawartosc = re.sub(r'[^\w\s]', '', zawartosc) #ta linia umożliwia aby słowa mogły zaczynać się z małej lub dużej
+slowa = zawartosc.split() #rodzielamy słowa
 
-
+#dodajemy licznik do każdego podzielnego słowa, jeśli pojawia się drugi raz, dodaj +1
 licznik = {}
 for slowo in slowa:
     if slowo in licznik:
@@ -16,8 +18,9 @@ for slowo in slowa:
     else:
         licznik[slowo] = 1
 
-szukane_slowo = input("wpisz słowo: ")
+szukane_slowo = input("wpisz słowo: ") #input na wpisanie słowa którego użytkownik potrzebuje
 
+#print który wyświetla słowo oraz jego liczebność w tekscie, dodatkowo odpowiedz zwrotna, gdyby słowa miało nie być
 if szukane_slowo in licznik:
     print(f"słowo '{szukane_slowo}' występuje {licznik[szukane_slowo]} razy.")
 else:
